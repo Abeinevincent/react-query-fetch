9d9066144b1ca0c0bc8f37a295a062276d39db15
Abeinevincent <abeinevincent@gmail.com>
2022-12-20 14:09:40 +0300

    refetch-on-window-focus-and-on-reconnect

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import App from './App'
 import './index.css'
-
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // refetchOnWindowFocus: true, ->Uncomment to enable it globally
+      // refetchOnReconnect: true ->Uncomment to enable it globally
+    }
+  }
+}) 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <QueryClientProvider client={new QueryClient}>
+  <QueryClientProvider client={queryClient}>
     <App />
   </QueryClientProvider>
 )
